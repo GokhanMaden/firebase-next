58d601dba3a8b9eb6549a457b979279d30ff0ac2
GokhanMaden <gokhanmaden1@gmail.com>
2022-12-08 00:16:26 +0100

    Add Server-side-rendering on slug file

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,39 +1,26 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 
-import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from '../../helpers/api-utils';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function FilteredEventsPage() {
-  const router = useRouter();
+function FilteredEventsPage({ events, hasError, date }: any) {
+  // const filterData = router.query.slug;
 
-  const filterData = router.query.slug;
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>;
+  // }
 
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
-  }
+  // const filteredYear = +filterData[0];
+  // const filteredMonth = +filterData[1];
 
-  const filteredYear = +filterData[0];
-  const filteredMonth = +filterData[1];
-
-  if (
-    isNaN(filteredYear) ||
-    isNaN(filteredMonth) ||
-    filteredYear > 2030 ||
-    filteredYear < 2021 ||
-    filteredMonth < 1 ||
-    filteredMonth > 12
-  ) {
+  if (hasError) {
     return <p>Invalid Filter. Please adjust your values!</p>;
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: filteredYear,
-    month: filteredMonth,
-  });
+  const filteredEvents = events;
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
@@ -48,13 +35,57 @@ function FilteredEventsPage() {
     );
   }
 
-  const date = new Date(filteredYear, filteredMonth - 1);
+  const selectedDate = new Date(date.year, date.month - 1);
   return (
     <>
-      <ResultsTitle date={date} />
+      <ResultsTitle date={selectedDate} />
       <EventList list={filteredEvents} />
     </>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const { params } = context;
+
+  const filterData = params.slug;
+
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredYear > 2030 ||
+    filteredYear < 2021 ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return {
+      props: {
+        hasError: true,
+      },
+      // notFound: true,
+      // redirect: {
+      //   destination: '/error'
+      // }
+    };
+  }
+
+  const filteredEvents = await getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  return {
+    props: {
+      events: filteredEvents,
+      hasError: false,
+      date: {
+        year: filteredYear,
+        month: filteredMonth,
+      },
+    },
+  };
 }
 
 export default FilteredEventsPage;
